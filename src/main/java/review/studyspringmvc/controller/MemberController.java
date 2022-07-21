ec9d5692aa95c35adbc167f592c21e742de48008
cszzang89@gamail.com <cszzang89@gmail.com>
2022-07-21 21:14:34 +0900

    @PathVariable

--- src/main/java/review/studyspringmvc/controller/MemberController.java
+++ src/main/java/review/studyspringmvc/controller/MemberController.java
@@ -1,18 +1,48 @@
 package review.studyspringmvc.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 //@Controller
 @RestController
 public class MemberController {
 
-    @RequestMapping(value = {"/members","/users","/memberlist"})
+    @RequestMapping(value = {"/members","/users","/memberlist"},method = RequestMethod.GET)
     public String members() {
         log.info("call this");
         return "members";
     }
+    /*
+    @RequestMapping(value = "/mapping_method",method = RequestMethod.GET)
+    @RequestMapping(value = "/mapping_method",method = RequestMethod.POST)
+    @RequestMapping(value = "/mapping_method",method = RequestMethod.PATCH)
+    @RequestMapping(value = "/mapping_method",method = RequestMethod.PUT)
+    @RequestMapping(value = "/mapping_method",method = RequestMethod.DELETE)
+     */
+    /**
+     * 편리한 축약 애노테이션 (코드보기) * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+
+    @GetMapping("/membermapping")
+    public String memberMapping(){
+        log.info("memberMapping");
+        return "membermapping";
+    }
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * @PathVariable("userId") String userId -> @PathVariable userId */
+    @GetMapping("/membermapping/{userId}")
+    public String memberMappingPath(@PathVariable("userId") String data){
+        log.info("memberMappingPath userId ={}",data);
+        return "members path = "+data;
+    }
+
+
+
 }
