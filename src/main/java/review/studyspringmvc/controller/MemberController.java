18da3e2f916789520101c87b2457a3d6ae45488f
cszzang89@gamail.com <cszzang89@gmail.com>
2022-07-22 16:45:36 +0900

    요청매핑

--- src/main/java/review/studyspringmvc/controller/MemberController.java
+++ src/main/java/review/studyspringmvc/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MemberController {
 
-    @RequestMapping(value = {"/members","/users","/memberlist"},method = RequestMethod.GET)
+    @RequestMapping(value = {"/members", "/users", "/memberlist"}, method = RequestMethod.GET)
     public String members() {
         log.info("call this");
         return "members";
@@ -20,8 +20,11 @@ public class MemberController {
     @RequestMapping(value = "/mapping_method",method = RequestMethod.PUT)
     @RequestMapping(value = "/mapping_method",method = RequestMethod.DELETE)
      */
+
     /**
-     * 편리한 축약 애노테이션 (코드보기) * @GetMapping
+     * 편리한 축약 애노테이션
+     *
+     * @GetMapping
      * @PostMapping
      * @PutMapping
      * @DeleteMapping
@@ -29,20 +32,80 @@ public class MemberController {
      */
 
     @GetMapping("/membermapping")
-    public String memberMapping(){
+    public String memberMapping() {
         log.info("memberMapping");
         return "membermapping";
     }
+
     /**
      * PathVariable 사용
      * 변수명이 같으면 생략 가능
-     * @PathVariable("userId") String userId -> @PathVariable userId */
-    @GetMapping("/membermapping/{userId}")
-    public String memberMappingPath(@PathVariable("userId") String data){
-        log.info("memberMappingPath userId ={}",data);
-        return "members path = "+data;
+     *
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     */
+    @GetMapping("/memberMapping/{userId}")
+    public String memberMappingPath(@PathVariable("userId") String data) {
+        log.info("memberMappingPath userId ={}", data);
+        return "members path = " + data;
     }
 
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/memberMapping/{userId}/address/{location}/post/{postNumber}")
+    public String memberMappingPath2(@PathVariable("userId") String data,
+                                     @PathVariable String location,
+                                     @PathVariable int postNumber) {
+        log.info("memberMappingPath userId ={}, location={}", data, location);
+        return "members path = " + data + ", location =" + location + ", postNumber =" + postNumber;
+    }
 
+    /**
+     * 파라미터로 추가 매핑
+     * params="isWorking",
+     * params="!isWorking"
+     * params="isWorking=isWorking"
+     * params="isWorking!=isWorking" (! = )
+     * params = {"mode=debug","data=good"}
+     */
+    @GetMapping(value = "/memberMapping-param", params = "isWorking")
+    public String memberMappingParam() {
+        log.info("memberMapping-param");
+        return "memberMapping-param";
+    }
 
+    /**
+     * 특정 헤더로 추가 매핑
+     * isWorking
+     */
+    @GetMapping(value = "/memberMapping-header", headers = "this=isWorking")
+    public String memberMappingHeader() {
+        log.info("memberMappingHeader");
+        return "memberMappingHeader - is Working";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type * consumes="application/json"
+     * * consumes="!application/json"
+     * * consumes="application/*"
+     * * consumes="*\/*"
+     * * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/memberMapping-consume", consumes = "application/json")
+    public String memberMappingConsumes() {
+        log.info("memberMappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type * produces = "text/html"
+     * produces = "!text/html" * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/memberMapping-produce", produces = "text/html")
+    public String memberMappingProduces() {
+        log.info("memberMappingProduces");
+        return "ok";
+    }
 }
+
