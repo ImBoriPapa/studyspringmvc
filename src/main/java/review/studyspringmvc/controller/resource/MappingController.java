57e4c53f53405f33e4eb3dc67d928f2c1dc92683
cszzang89@gamail.com <cszzang89@gmail.com>
2022-07-25 16:48:40 +0900

    HTTP RequestHeader 기본,조회

---
+++ src/main/java/review/studyspringmvc/controller/resource/MappingController.java
@@ -0,0 +1,38 @@
+package review.studyspringmvc.controller.resource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/mapping/items")
+public class MappingController {
+
+    @GetMapping
+    public String items() {
+        return "items";
+    }
+
+    @PostMapping
+    public String saveItem() {
+        return "saveItem";
+    }
+
+    @GetMapping("/{itemId}")
+    public String findItem(@PathVariable String itemId) {
+        return "findItem =" + itemId;
+    }
+
+    @PatchMapping("/{itemId}")
+    public String updateItem(@PathVariable String itemId) {
+        return "update Item ="+itemId;
+    }
+
+    @DeleteMapping("/{itemId}")
+    public String deleteItem(@PathVariable String itemId) {
+        return "delete item=" + itemId;
+    }
+
+
+
+}
--- src/main/java/review/studyspringmvc/controller/resource/MemberController.java
+++ src/main/java/review/studyspringmvc/controller/resource/MemberController.java
@@ -1,4 +1,4 @@
-package review.studyspringmvc.controller;
+package review.studyspringmvc.controller.resource;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
