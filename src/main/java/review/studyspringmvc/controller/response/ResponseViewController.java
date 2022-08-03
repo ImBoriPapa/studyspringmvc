ba436781bf22e78380732bd66538b81dca3a6f57
cszzang89@gamail.com <cszzang89@gmail.com>
2022-08-03 17:02:19 +0900

    Response static,view

---
+++ src/main/java/review/studyspringmvc/controller/response/ResponseViewController.java
@@ -0,0 +1,30 @@
+package review.studyspringmvc.controller.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView modelAndView = new ModelAndView("response/home")
+                .addObject("data","welcome home!");
+        return modelAndView;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "welcome home!");
+        return "response/home";
+    }
+
+    @RequestMapping("/response/home")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "welcome home!");
+    }
+}
+
